f46120bc7ca804b7d3dd1ad94ded74bf3a32ddce
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-22 16:45:01 -0500

    Exercise complete, ready for review

--- array-methods.js
+++ array-methods.js
@@ -3,32 +3,30 @@
  */
 let planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
 
-/*
- Use the forEach method to add the name of each planet
- to a div element in your HTML with an id of "planets".
- */
 let el = document.getElementById("planets");
 planets.forEach((e)=>{
    el.innerHTML = planets
-
-
-    let capital_planets = planets.map((a)=>{
-        console.log(a)
-        a.charAt([0]).toUpperCase()
-        console.log(a)
-
-    })
-    console.log(capital_planets);
 });
 
-// Use the map method to create a new array where the first letter of each planet is capitalized
+
+let capital_planets = planets.map((e)=>{
+    return e.charAt([0]).toUpperCase() + e.substr(1, 7)
+});
+console.log(capital_planets);
 
 
+let e_planets = [];
+capital_planets.filter((a)=>{
+    if (a.toString().includes('e')){
+        e_planets.push(a)
+    }
+});
+console.log(e_planets);
 
 
+let words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
 
-// Use the filter method to create a new array that contains planets with the letter 'e'
-
-// Use the reduce method to create a sentence from the words in the following array
-
-var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
+let sentence = words.reduce((a, b)=>{
+  return `${a} ${b}`
+});
+console.log(sentence);
